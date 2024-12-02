a1ac1942b59ed481e343029fbd939deb7b100996
SanttuA <santtualatalo@gmail.com>
2024-12-02 15:19:23 +0200

    Changed frontend main heading text (#25)
    Frontend main heading now indicates that this is a test version.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <main className="w-screen flex justify-center items-center">
       <div className="py-10 space-y-2 lg:space-y-10 w-[90%] lg:w-[60rem]">
-        <h1 className="text-4xl font-bold text-white">Laatukäsikirja-chatbot</h1>
+        <h1 className="text-4xl font-bold text-white">Laatukäsikirja-chatbot, testiversio</h1>
         <div className="h-[65vh] flex">
         {process.env.USE_TUNNISTAMO && !session ? (
           <p className="text-white text-lg">Tervetuloa käyttämään Laatukäsikirja chatbottia. Aloita käyttö kirjautumalla ensin sisään.</p>

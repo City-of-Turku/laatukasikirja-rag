725da59bb951b13c35477a85db4ba319a0d05a32
juuso-j <juuso.jokiniemi@turku.fi>
2024-12-10 09:07:51 +0200

    Refactor

---
+++ backend/app/api/routers/data_files copy.py
@@ -0,0 +1,66 @@
+import logging
+import os
+import json
+from pathlib import Path
+from functools import lru_cache
+
+from fastapi import APIRouter, Depends,  HTTPException, status
+
+from app.auth import get_api_token
+from app.api.routers.models import DataFilesData, SourceNodes
+
+data_files_router = r = APIRouter()
+
+logger = logging.getLogger("uvicorn")
+
+
+@lru_cache
+def get_data_files(json_file: Path) -> dict | json.JSONDecodeError:
+    try:
+        with open(json_file, "r") as f:
+            return json.load(f)
+    except json.JSONDecodeError as e:
+        return e
+
+def raise_error(detail_msg: str, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR):
+    # Clear cache in errors, this allows fixing errors without restarting the server.        
+    get_data_files.cache_clear()    
+    logger.error(detail_msg)
+    raise HTTPException(
+        status_code=status_code,
+        detail=detail_msg
+    )
+
+@r.get("/data_files")
+#def data_files(api_token: str = Depends(get_api_token)) -> DataFilesData:
+
+def data_files() -> DataFilesData:
+    data_dir = Path.cwd() / "storage"
+    json_file = data_dir / "docstore.json"
+
+    if not data_dir.exists() or not data_dir.is_dir():
+        raise_error("'storage' directory not found.")
+
+    if not os.path.exists(json_file):
+        raise_error("'docstore.json' not found.")       
+
+    ret_data = get_data_files(json_file)
+    
+    if type(ret_data) == dict:
+        files = None
+        try:
+            docstore_data = ret_data["docstore/data"]
+            files = list(set([docstore_data[key]["__data__"]["metadata"]["file_name"] for key in docstore_data.keys()]))
+        except KeyError as e:
+            raise_error(f"KeyError: {str(e)}")        
+        ret = []
+        for key in docstore_data.keys():
+            file_name = docstore_data[key]["__data__"]["metadata"]["file_name"]
+            metadata = {"file_name": file_name, "file_path": docstore_data[key]["__data__"]["metadata"]["file_path"]}
+            url = SourceNodes.get_url_from_metadata(metadata)
+            ret.append({"file_name": file_name, "url":url})
+        #breakpoint()
+        return {"files": ret}   
+    else:
+        raise_error( f"Failed to decode JSON from '{json_file}'")
+     

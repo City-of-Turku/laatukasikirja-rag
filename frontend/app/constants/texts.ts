290c476ac2e6c2d9bed8e0fc4aa0d27ce4983139
SanttuA <santtualatalo@gmail.com>
2024-12-11 13:04:04 +0200

    Changed frontend key texts to come from constants file

---
+++ frontend/app/constants/texts.ts
@@ -0,0 +1,14 @@
+export const SITE_TEXTS = {
+  meta: {
+    title: "Laatukäsikirja-Bot",
+    description: "Turun Ammatti-instituutin chatbot",
+  },
+  homepage: {
+    heading: "Laatukäsikirja-chatbot, testiversio",
+    welcomeMsg:
+      "Tervetuloa käyttämään Laatukäsikirja-chatbottia. Aloita käyttö kirjautumalla ensin sisään.",
+  },
+  changeLogPage: {
+    metaTitle: "Laatukäsikirja-Bot | Muutoshistoria",
+  },
+};
